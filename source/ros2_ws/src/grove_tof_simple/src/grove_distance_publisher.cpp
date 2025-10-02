561241d98b864fcce0b35188419930cd77c1af47
pang0620 <129kjy@gmail.com>
2025-10-02 11:26:18 +0900

    grove_distance_subscriber added

--- source/ros2_ws/src/grove_tof_simple/src/grove_distance_publisher.cpp
+++ source/ros2_ws/src/grove_tof_simple/src/grove_distance_publisher.cpp
@@ -1,13 +1,14 @@
 // basic c++ header files
-#include <memory>
-#include <string>
-#include <chrono>
+#include <memory>  // std::shared_ptr
+#include <chrono>  // std::chrono_literals
 
 // for serial port
+#if 0
 #include <fcntl.h>
 #include <unistd.h>
 #include <termios.h>
 #include <string>
+#endif
 
 // ROS2 node, range message
 #include "rclcpp/rclcpp.hpp"
@@ -21,24 +22,16 @@ public:
   GroveDistancePublisher()
   : Node("grove_distance_publisher")
   {
-    //Q. this name "publisher_" is used in ROS2 network? then this naming doesn't seems to be good.
-    //A. this name is used only inside this class. so it's OK.
     publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("distance", 10);
 
     timer_ = this->create_wall_timer(
         1000ms, std::bind(&GroveDistancePublisher::timer_callback, this)
-        //Q. can't I write as "1s" directly here? isn't it chorno_literals feature?
-        //A. you can write as "1s" directly here if you include "using namespace std::chrono_literals;" at the top of the file.
-        //Q. why std::bind is needed here? Isn't lambda() more modern?
-        //A. because we need to pass "this" pointer to member function
-        //A. lambda can be used, but std::bind is more readable in this case
-        //와 코파일럿이 답도 해줌, 근데 이 답이 맞는지는 잘 모르겠음
     );
   }
 
   ~GroveDistancePublisher()
   {
-
+    RCLCPP_INFO(this->get_logger(), "Shutting down grove_distance_publisher node.");
   }
 
 private:
@@ -47,8 +40,6 @@ private:
     auto message = std_msgs::msg::Float32MultiArray();
 
     message.data.resize(4);
-    //Q. why not request, it's resize?
-    //A. because Float32MultiArray is not a vector, it's a message type
 
     //TODO: these are just some random values, replace with actual sensor readings
     message.data[0] = static_cast<float>(rand() % 4000) / 100.0; // Simulated distance in cm
@@ -63,8 +54,6 @@ private:
   }
 
   rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publisher_;
-  //Q. what is this long type?
-  //A. it's a shared pointer to a publisher of Float32MultiArray message type
   rclcpp::TimerBase::SharedPtr timer_;
 
 };
@@ -73,11 +62,6 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<GroveDistancePublisher>());
-    //Q. what is spin?
-    //A. spin() is a function that keeps the node alive and processing callbacks(blocking)
     rclcpp::shutdown();
-    //Q. why shutdown is here?
-    //A. shutdown() is called to clean up resources before exiting the program
-    //Q. My knowledge: this is called by ^c, right?
     return 0;
 }
