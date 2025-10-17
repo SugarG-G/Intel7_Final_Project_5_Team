fb724bbd2622c379c676d1c175065963b3ff34ce
pang0620 <129kjy@gmail.com>
2025-10-17 20:51:50 +0900

    update 251017

--- source/turtlebot3_ws/src/turtlebot3_applications/turtlebot3_parallel_follower/src/parking_a_lot.cpp
+++
@@ -1,9 +0,0 @@
-#include "rclcpp/rclcpp.hpp"
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  // In the future, a node will be created and spun here.
-  rclcpp::shutdown();
-  return 0;
-}
